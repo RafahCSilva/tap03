a05375f91b1fedba5f31ca6cfde0e34cea5847b5
RafahCSilva <rafahcsilva@gmail.com>
2018-04-11 23:06:47 -0300

    add enunciado

---
+++ src/rafaelcardoso_tap03.cpp
@@ -0,0 +1,20 @@
+/**
+ * @author Rafael Cardoso da Silva 21048012
+ * Tecnicas Avancadas de Programacao – 2018.Q1
+ * Projeto 2: Domino
+ *
+ * Compile com C++ 17:
+ *   g++ rafaelcardoso_tap03.cpp -o rafaelcardoso_tap03.exe -std=c++17
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+int main() {
+
+    std::cout << "oi" << std::endl;
+
+    return 0;
+}
