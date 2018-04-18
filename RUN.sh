598a92f10e5a653e4ef1a279ac7de1448ed5f8a8
RafahCSilva <rafahcsilva@gmail.com>
2018-04-18 19:48:04 -0300

    add scripts de correção do professor

--- RUN.sh
+++ RUN.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 g++ src/rafaelcardoso_tap03.cpp -o src/rafaelcardoso_tap03.exe -std=c++17
- ./src/rafaelcardoso_tap03.exe < test/in.txt
 
+./src/rafaelcardoso_tap03.exe < test/in.txt
+
+
+#cp ./src/rafaelcardoso_tap03.exe ./grammar/a.out
+#cd ./grammar
+#./grade.bash
